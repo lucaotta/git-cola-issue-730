ecaea392d9ebe49c3fd952b2d9f6d3d2ff4ba181
Luca Ottaviano <lottaviano@develer.com>
2017-10-19 07:16:58 +0200

    test

---
+++ C++/Support/FactoryTest/factory_test.cpp
@@ -0,0 +1,12 @@
+#include "factory_test.h"
+#include "C++/AlarmManagement/alarm_mng.h"
+
+PowerupStateMachine::PowerupStateMachine() : QObject(parent)
+{
+    m_state = Normal;
+    if ( frsInstall )
+        m_state = WelcomeScreen;
+    if ( !fctTestDone )
+        m_state = FactoryStep1;
+    m_alarms = alarms;
+}
